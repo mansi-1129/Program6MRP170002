dfd76d95b6f51e4a47f879b55383e1ab4b04d038
mansi-1129 <mansipedireddy@yahoo.com>
2020-04-30 17:29:37 -0500

    uploading files

--- example1.cc
+++ example1.cc
@@ -2,31 +2,27 @@
  * Usage of CDK Matrix
  *
  * File:   example1.cc
- * Author: Stephen Perkins
- * Email:  stephen.perkins@utdallas.edu
+ * Date:   04/29/20
+ * Author: Mansi Pedireddy 
+ * Email:  mrp170002@utdallas.edu
+ * Course: CS 3377.501 Spring2020 
+ * Version:1.0
+ * Copyright: 2020, All Rights Reserved
+ * Description: Creates and displays matrix
  */
 
-#include <iostream>
-#include "cdk.h"
+#include "headerfile.h"
+//#include "binFileIOExample.cc"
 
-
-#define MATRIX_WIDTH 3
-#define MATRIX_HEIGHT 3
-#define BOX_WIDTH 15
-#define MATRIX_NAME_STRING "Test Matrix"
-
-using namespace std;
-
-
-int main()
+int contents(string matrixDisplay[MATRIX_HEIGHT+1][MATRIX_WIDTH+1])
 {
 
   WINDOW	*window;
   CDKSCREEN	*cdkscreen;
   CDKMATRIX     *myMatrix;           // CDK Screen Matrix
 
-  const char 		*rowTitles[MATRIX_HEIGHT+1] = {"R0", "R1", "R2", "R3"};
-  const char 		*columnTitles[MATRIX_WIDTH+1] = {"C0", "C1", "C2", "C3"};
+  const char 		*rowTitles[MATRIX_HEIGHT+1] = {"0","a", "b", "c","d","e"};
+  const char 		*columnTitles[MATRIX_WIDTH+1] = {"0","a", "b", "c"};
   int		boxWidths[MATRIX_WIDTH+1] = {BOX_WIDTH, BOX_WIDTH, BOX_WIDTH, BOX_WIDTH};
   int		boxTypes[MATRIX_WIDTH+1] = {vMIXED, vMIXED, vMIXED, vMIXED};
 
@@ -44,29 +40,37 @@ int main()
   /*
    * Create the matrix.  Need to manually cast (const char**) to (char **)
   */
-  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT,
+  myMatrix = newCDKMatrix(cdkscreen, CENTER, CENTER, MATRIX_HEIGHT, MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_WIDTH,
 			  MATRIX_NAME_STRING, (char **) columnTitles, (char **) rowTitles, boxWidths,
 				     boxTypes, 1, 1, ' ', ROW, true, true, false);
 
-  if (myMatrix ==NULL)
-    {
+  if (myMatrix ==NULL){
       printf("Error creating Matrix\n");
-      _exit(1);
+      exit(1);
     }
 
-  /* Display the Matrix */
-  drawCDKMatrix(myMatrix, true);
-
-  /*
-   * Dipslay a message
-   */
-  setCDKMatrixCell(myMatrix, 2, 2, "Test Message");
-  drawCDKMatrix(myMatrix, true);    /* required  */
-
-  /* so we can see results */
-  sleep (10);
 
 
-  // Cleanup screen
+drawCDKMatrix(myMatrix, true);
+
+int hMAX = MATRIX_HEIGHT+1 ;
+
+for(int i = 1; i < hMAX; i++)
+{
+
+int wMAX = MATRIX_WIDTH+1 ;
+
+for (int j = 1; j < wMAX; j++)
+{
+setCDKMatrixCell(myMatrix, i, j, matrixDisplay[i][j].c_str());
+}
+}
+
+drawCDKMatrix(myMatrix, true);
+
+
+  sleep(10);
   endCDK();
+  return(0);
+
 }
---
+++ headerfile.h
@@ -0,0 +1,37 @@
+
+
+
+// File:   headerfile.h
+// Date:   04/29/20
+// Author: Mansi Pedireddy 
+// Email:  mrp170002@utdallas.edu
+// Course: CS 3377.501 Spring2020 
+// Version:1.0
+//Copyright: 2020, All Rights Reserved
+ 
+// Description: header file for example1.cc and binFileIOExample.cc
+
+ #ifndef HEADERFILE_H
+#define HEADERFILE_H
+
+#include <iostream>     
+#include <fstream>      
+#include <string>       
+#include <sstream>      
+#include <stdio.h>      
+#include <stdlib.h>     
+#include <stdint.h>	
+#include "cdk.h"
+#include <string>       
+#include <sstream>      
+#include <stdio.h>  
+#define MATRIX_WIDTH 3
+#define MATRIX_HEIGHT 5
+#define BOX_WIDTH 20
+#define MATRIX_NAME_STRING "Binary File Contents"
+
+
+using namespace std;
+
+int contents(string [MATRIX_HEIGHT+1 ][MATRIX_WIDTH+1 ]);
+#endif
